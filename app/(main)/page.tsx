97f1775f0c85bd00578cdc687f1198bdd96f1abf
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-07-04 15:55:18 +0300

    Minor fix

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import { Header } from '@/components/main/Header'
 import { Preview } from '@/components/main/Preview'
 import { Controls } from '@/components/main/Controls'
@@ -13,6 +15,17 @@ export default function Main() {
       <Controls />
       <Achievements />
       <Questions />
+
+      <div className="flex justify-center mt-12">
+        <Image
+          className="w-full lg:w-[512px] rounded-xl overflow-hidden"
+          src="/about-us.gif"
+          alt="about"
+          width={100}
+          height={100}
+        />
+      </div>
+
       <Copyright />
     </main>
   )
