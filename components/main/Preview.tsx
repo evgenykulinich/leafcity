a81f87db388f64a23ee30ca9b1ad467e243986e1
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-07-04 14:41:49 +0300

    Add preview image

--- components/main/Preview.tsx
+++ components/main/Preview.tsx
@@ -1,6 +1,15 @@
+import Image from 'next/image'
+
 export function Preview() {
   return (
-    <section>
+    <section className="relative">
+      <Image
+        className="hidden xl:block absolute right-0 top-0 xl:size-[576px]"
+        src="/animal.png"
+        alt="animal"
+        height={1024}
+        width={1024}
+      />
       <h1 className="font-bold text-[42px] lg:text-[64px] leading-tight mt-6 lg:mt-20">
         Уникальный{' '}
         <span className="text-blue">
