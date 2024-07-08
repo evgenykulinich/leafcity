05f1a5d1e1a7f329a363377a124f1848d6d787d6
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-07-08 12:36:24 +0300

    Fix og image path

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
   openGraph: {
     images: [
       {
-        url: '/opengraph-image.png'
+        url: `${url.baseUrl}opengraph-image.png`
       }
     ],
     description:
