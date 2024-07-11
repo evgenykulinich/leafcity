56ca430a6281db44befb9f889be0cf57e3d2eb16
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-07-12 02:55:32 +0300

    Added ui active:scale improvement to header logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,10 @@ import { Navigation } from '@/components/Navigation'
 export function Header() {
   return (
     <header className="flex items-center justify-between border-b-2 border-white/10 pb-4">
-      <Link className="flex items-center transition duration-300 hover:scale-110" href="/">
+      <Link
+        className="flex items-center transition duration-300 hover:scale-105 lg:active:scale-100"
+        href="/"
+      >
         <Image
           className="mr-4 w-[40px] cursor-pointer"
           src="/logo/lc.png"
