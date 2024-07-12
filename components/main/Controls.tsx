15fbf87ced6d8acca306e5f66fb952746f60308e
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-07-12 07:16:14 +0300

    Added animation to click-to-copy ip button

--- components/main/Controls.tsx
+++ components/main/Controls.tsx
@@ -15,19 +15,28 @@ const copyIpMessage = {
 
 export function Controls() {
   const [copyMessage, setCopyMessage] = useState(copyIpMessage.default)
+  const [animate, setAnimate] = useState(false)
 
   const toggleCopyIp = async () => {
     try {
       if (copyMessage === copyIpMessage.default) {
         await navigator.clipboard.writeText(copyIpMessage.textToCopy)
         setCopyMessage(copyIpMessage.success)
+        setAnimate(true)
+        setTimeout(() => setAnimate(false), 200)
+        return
       }
 
       if (copyMessage === copyIpMessage.success || copyMessage === copyIpMessage.error) {
         setCopyMessage(copyIpMessage.default)
       }
+
+      setAnimate(true)
+      setTimeout(() => setAnimate(false), 200)
     } catch {
       setCopyMessage(copyIpMessage.error)
+      setAnimate(true)
+      setTimeout(() => setAnimate(false), 200)
     }
   }
 
@@ -36,7 +45,7 @@ export function Controls() {
       <Link target="_blank" href={url.discord}>
         <Button
           variant="default"
-          className="mb-4 w-full rounded-xl bg-green px-16 py-10 text-[20px] font-bold hover:bg-green/70 lg:mr-10 lg:w-[300px]"
+          className="mb-4 w-full rounded-xl bg-green px-16 py-10 text-[20px] font-bold hover:bg-green/70 lg:mr-10 lg:w-[300px] lg:active:bg-green/50"
         >
           Начать играть
         </Button>
@@ -48,7 +57,7 @@ export function Controls() {
           onClick={toggleCopyIp}
           className="block h-[80px] w-full rounded-xl border-green text-[20px] hover:bg-green/10 lg:w-[300px]"
         >
-          <span>{copyMessage}</span>
+          <span className={animate ? 'fade-in' : ''}>{copyMessage}</span>
         </Button>
       </div>
     </div>
