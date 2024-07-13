1b512698b66500fb4d907635ee0970bc361821d8
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-07-13 15:09:32 +0300

    Fixed web map url

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -20,7 +20,7 @@ export default function MapPage() {
       </div>
       <div className="flex-1 overflow-hidden">
         <iframe
-          src="http://194.147.90.247:83"
+          src="https://leafcity.ru/backend/survival"
           className="h-[calc(100dvh-72px)] w-full border-0 sm:h-full"
           title="Map"
         />
