a1cbb4d848a0c3f0f7075ae8c6ebb41331aa27d8
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-08-01 19:22:30 +0300

    Added world selection buttons

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,39 +1,9 @@
-import { Header } from '@/components/Header'
-import { routes } from '@/constants/routes'
-
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Карта',
-  description:
-    'Интерактивная карта игрового сервера. Узнайте расположение построек и территорий. Полезно для новичков и опытных игроков',
-  openGraph: {
-    title: 'LEAF CITY - Карта',
-    images: [
-      {
-        url: `${routes.baseUrl}/og/map.png`,
-        width: 1200,
-        height: 630,
-        alt: 'LEAF CITY - Карта'
-      }
-    ],
-    description:
-      'Интерактивная карта игрового сервера. Узнайте расположение построек и территорий. Полезно для новичков и опытных игроков',
-    type: 'website'
-  }
-}
+import MapWorlds from '@/components/map/MapWorlds'
 
 export default function MapPage() {
-  const noCacheUrl = `https://leafcity.ru/backend/survival?nocache=${new Date().getTime()}`
-
   return (
     <div className="flex h-screen w-screen flex-col overflow-x-hidden">
-      <div className="fixed mx-auto w-full max-w-screen-2xl touch-none px-4 pt-4 lg:static lg:px-8">
-        <Header />
-      </div>
-      <div className="fixed bottom-0 w-full flex-1 overflow-hidden">
-        <iframe src={noCacheUrl} className="h-[calc(100dvh-65px)] w-full border-0" title="Map" />
-      </div>
+      <MapWorlds />
     </div>
   )
 }
---
+++ components/map/MapWorlds.tsx
@@ -0,0 +1,120 @@
+'use client'
+
+import { useState } from 'react'
+import Image from 'next/image'
+import { clsx } from 'clsx'
+
+import { Card } from '@/components/ui/card'
+import { Header } from '@/components/Header'
+import { Copyright } from '@/components/copyright/Copyright'
+
+const worlds = [
+  {
+    title: 'survival',
+    description: 'Мир выживания',
+    color: 'purple',
+    icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiM3Mjg5REEiIHN0cm9rZS13aWR0aD0iMS41IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXNwcm91dCI+PHBhdGggZD0iTTcgMjBoMTAiLz48cGF0aCBkPSJNMTAgMjBjNS41LTIuNS44LTYuNCAzLTEwIi8+PHBhdGggZD0iTTkuNSA5LjRjMS4xLjggMS44IDIuMiAyLjMgMy43LTIgLjQtMy41LjQtNC44LS4zLTEuMi0uNi0yLjMtMS45LTMtNC4yIDIuOC0uNSA0LjQgMCA1LjUuOHoiLz48cGF0aCBkPSJNMTQuMSA2YTcgNyAwIDAgMC0xLjEgNGMxLjktLjEgMy4zLS42IDQuMy0xLjQgMS0xIDEuNi0yLjMgMS43LTQuNi0yLjcuMS00IDEtNC45IDJ6Ii8+PC9zdmc+'
+  },
+  {
+    title: 'farms',
+    description: 'Мир ферм',
+    color: 'green',
+    icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiM2N2M5NjAiIHN0cm9rZS13aWR0aD0iMS41IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXRyYWN0b3IiPjxwYXRoIGQ9Im0xMCAxMSAxMSAuOWExIDEgMCAwIDEgLjggMS4xbC0uNjY1IDQuMTU4YTEgMSAwIDAgMS0uOTg4Ljg0MkgyMCIvPjxwYXRoIGQ9Ik0xNiAxOGgtNSIvPjxwYXRoIGQ9Ik0xOCA1YTEgMSAwIDAgMC0xIDF2NS41NzMiLz48cGF0aCBkPSJNMyA0aDguMTI5YTEgMSAwIDAgMSAuOTkuODYzTDEzIDExLjI0NiIvPjxwYXRoIGQ9Ik00IDExVjQiLz48cGF0aCBkPSJNNyAxNWguMDEiLz48cGF0aCBkPSJNOCAxMC4xVjQiLz48Y2lyY2xlIGN4PSIxOCIgY3k9IjE4IiByPSIyIi8+PGNpcmNsZSBjeD0iNyIgY3k9IjE1IiByPSI1Ii8+PC9zdmc+'
+  },
+  {
+    title: 'resources',
+    description: 'Мир ресурсов',
+    color: 'blue',
+    icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiMyZTlmZDIiIHN0cm9rZS13aWR0aD0iMS41IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXBpY2theGUiPjxwYXRoIGQ9Ik0xNC41MzEgMTIuNDY5IDYuNjE5IDIwLjM4YTEgMSAwIDEgMS0zLTNsNy45MTItNy45MTIiLz48cGF0aCBkPSJNMTUuNjg2IDQuMzE0QTEyLjUgMTIuNSAwIDAgMCA1LjQ2MSAyLjk1OCAxIDEgMCAwIDAgNS41OCA0LjcxYTIyIDIyIDAgMCAxIDYuMzE4IDMuMzkzIi8+PHBhdGggZD0iTTE3LjcgMy43YTEgMSAwIDAgMC0xLjQgMGwtNC42IDQuNmExIDEgMCAwIDAgMCAxLjRsMi42IDIuNmExIDEgMCAwIDAgMS40IDBsNC42LTQuNmExIDEgMCAwIDAgMC0xLjR6Ii8+PHBhdGggZD0iTTE5LjY4NiA4LjMxNGExMi41MDEgMTIuNTAxIDAgMCAxIDEuMzU2IDEwLjIyNSAxIDEgMCAwIDEtMS43NTEtLjExOSAyMiAyMiAwIDAgMC0zLjM5My02LjMxOSIvPjwvc3ZnPg=='
+  }
+]
+
+export default function MapWorlds() {
+  const [activeWorld, setActiveWorld] = useState<string | null>(null)
+  const [buttonsVisible, setButtonsVisible] = useState(true)
+
+  const noCacheUrl = (world: string) =>
+    `https://leafcity.ru/backend/${world}?nocache=${new Date().getTime()}`
+
+  const handleButtonClick = (world: string) => {
+    setActiveWorld(world)
+    setButtonsVisible(false)
+  }
+
+  return (
+    <>
+      {buttonsVisible && (
+        <div className="flex flex-col pb-4 lg:h-dvh">
+          <div className="mx-auto w-full max-w-screen-2xl touch-none px-4 pt-4 lg:static lg:px-8">
+            <Header />
+          </div>
+          <div className="mt-8 flex h-full flex-col items-center justify-center gap-10 px-12 lg:mt-0 lg:flex-row lg:gap-20">
+            {worlds.map(world => (
+              <Card
+                className={clsx(
+                  `cursor-pointer rounded-2xl border-2 p-4 transition lg:p-6 lg:hover:scale-105 lg:active:scale-100`,
+                  {
+                    'bg-purple/10': world.color === 'purple',
+                    'bg-green/10': world.color === 'green',
+                    'bg-blue/10': world.color === 'blue'
+                  },
+                  {
+                    'hover:bg-purple/20': world.color === 'purple',
+                    'hover:bg-green/20': world.color === 'green',
+                    'hover:bg-blue/20': world.color === 'blue'
+                  },
+                  {
+                    'border-purple/40': world.color === 'purple',
+                    'border-green/40': world.color === 'green',
+                    'border-blue/40': world.color === 'blue'
+                  },
+                  {
+                    'hover:border-purple': world.color === 'purple',
+                    'hover:border-green': world.color === 'green',
+                    'hover:border-blue': world.color === 'blue'
+                  },
+                  {
+                    'text-purple': world.color === 'purple',
+                    'text-green': world.color === 'green',
+                    'text-blue': world.color === 'blue'
+                  }
+                )}
+                key={world.title}
+                onClick={() => handleButtonClick(world.title)}
+              >
+                <div className={`${world.title} size-[300px] rounded-2xl`}></div>
+                <div className="mt-4 flex items-center justify-center gap-1.5 text-xl lg:mt-6">
+                  <Image
+                    className="w-[25px] lg:w-[25px]"
+                    alt="Аватар"
+                    src={world.icon}
+                    width={180}
+                    height={180}
+                  />
+                  <p>{world.description}</p>
+                </div>
+              </Card>
+            ))}
+          </div>
+          <div className="mx-auto w-full max-w-screen-2xl px-4 pt-4 lg:px-8">
+            <Copyright />
+          </div>
+        </div>
+      )}
+      {!buttonsVisible && activeWorld && (
+        <div>
+          <div className="fixed mx-auto w-full max-w-screen-2xl touch-none px-4 pt-4 lg:static lg:px-8">
+            <Header />
+          </div>
+          <div className="fixed bottom-0 w-full flex-1 overflow-hidden">
+            <iframe
+              src={noCacheUrl(activeWorld)}
+              className="h-[calc(100dvh-65px)] w-full border-0"
+              title="Map"
+            />
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
