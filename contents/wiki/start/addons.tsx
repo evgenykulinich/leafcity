cce1f0d46c460aaabb59cf49af1f5be62df75ece
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-08-08 21:11:29 +0300

    Added color picker to nickcolor wiki page

--- contents/wiki/start/addons.tsx
+++ contents/wiki/start/addons.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 import { getLastUpdateDate } from '@/helpers/getLastUpdateDate'
 import { allowedMods, bannedMods, bannedPacks } from '@/data/wiki'
-import { InfoText } from '@/components/wiki/textVariations'
+import { InfoText } from '@/components/wiki/TextVariations'
 
 const updateDate = '3 Aug 2024'
 const sortedAllowedModsList = allowedMods.sort((prev, next) => (prev.name > next.name ? 1 : -1))
