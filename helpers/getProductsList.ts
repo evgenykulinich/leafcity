2689a3ba58e66ba647ef7a2e5817811c223cf76f
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-08-21 15:39:49 +0300

    Minor fix

--- helpers/getProductsList.ts
+++ helpers/getProductsList.ts
@@ -5,11 +5,7 @@ export async function getProductsList() {
     const getProductsUrl = `${process.env.NEXT_PUBLIC_API_URL}/shop/products`
     const productsResponse = await fetch(getProductsUrl, {
       method: 'GET',
-      cache: 'no-store',
-      headers: {
-        Authorization:
-          'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyYXRrZWEiLCJleHAiOjE3MjQ4NDg0MjR9.tSKnbdB-WhvvouQYSQc9neIqWG9WugnesZhbgWQXSnA4w32R6msSIgaFl6Eg6V_RXLbTKpLrfpWSZ2oQZGuSvg'
-      }
+      cache: 'no-store'
     })
 
     if (!productsResponse.ok) {
