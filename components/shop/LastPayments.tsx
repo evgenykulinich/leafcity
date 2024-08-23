46c89b40677166979286620fa0378a3b452a8b56
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-08-23 17:44:43 +0300

    Added empty data checkers

--- components/shop/LastPayments.tsx
+++ components/shop/LastPayments.tsx
@@ -3,14 +3,17 @@ import { getLastPaymentsList } from '@/helpers/getLastPayments'
 
 export const LastPayments = async () => {
   const lastPayments = await getLastPaymentsList()
-  if (lastPayments.length === 0) return
 
   return (
-    <section className="mt-16 overflow-hidden lg:mt-24">
-      <h2 className="text-center text-2xl lg:text-left">Последние покупки</h2>
-      <div className="mt-8 select-none lg:mt-12">
-        <PaymentCarousel lastPayments={lastPayments} />
-      </div>
-    </section>
+    <>
+      {lastPayments?.length ? (
+        <section className="mt-16 overflow-hidden lg:mt-24">
+          <h2 className="text-center text-2xl lg:text-left">Последние покупки</h2>
+          <div className="mt-8 select-none lg:mt-12">
+            <PaymentCarousel lastPayments={lastPayments} />
+          </div>
+        </section>
+      ) : null}
+    </>
   )
 }
