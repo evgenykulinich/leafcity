67451d22a2c1a16951a71024d9ae68556b6e4ff9
Zhenya Kulinich <kulinichevgeny@gmail.com>
2024-08-29 15:53:18 +0300

    Removed last payments section

--- components/shop/LastPayments.tsx
+++ components/shop/LastPayments.tsx
@@ -4,16 +4,18 @@ import { getLastPaymentsList } from '@/helpers/getLastPayments'
 export const LastPayments = async () => {
   const lastPayments = await getLastPaymentsList()
 
-  return (
-    <>
-      {lastPayments?.length ? (
-        <section className="mt-16 overflow-hidden lg:mt-24">
-          <h2 className="text-center text-2xl lg:text-left">Последние покупки</h2>
-          <div className="mt-8 select-none lg:mt-12">
-            <PaymentCarousel lastPayments={lastPayments} />
-          </div>
-        </section>
-      ) : null}
-    </>
-  )
+  return null
+
+  // return (
+  //   <>
+  //     {lastPayments?.length ? (
+  //       <section className="mt-16 overflow-hidden lg:mt-24">
+  //         <h2 className="text-center text-2xl lg:text-left">Последние покупки</h2>
+  //         <div className="mt-8 select-none lg:mt-12">
+  //           <PaymentCarousel lastPayments={lastPayments} />
+  //         </div>
+  //       </section>
+  //     ) : null}
+  //   </>
+  // )
 }
